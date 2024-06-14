457ded0c607d8ad3a81838f1ec183bed7ff1fd41
jujusinhodoscapa <166841741+jujusinhodoscapa@users.noreply.github.com>
2024-06-14 13:52:01 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    for (let index = 0; index < 10; index++) {
+        contador += -1
+        basic.showNumber(contador)
+        basic.pause(100)
+    }
+})
+let contador = 0
+contador = 10
+basic.showNumber(contador)
+basic.forever(function () {
+	
+})
